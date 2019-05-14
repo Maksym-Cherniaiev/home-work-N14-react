10ed602811df147246afd67efe454e9690eeda7f
Maksym <likeran2@gmail.com>
2019-05-14 21:39:06 +0300

    Updates

---
+++ precache-manifest.41ed4a7b4e3a146b489ed91431bfe337.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5555622bf262e1241300cee2a7b824a9",
+    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/index.html"
+  },
+  {
+    "revision": "d3501750e989824b6bff",
+    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/2.0259027c.chunk.js"
+  },
+  {
+    "revision": "647f2cdd7801a68d974b",
+    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/main.c6bcfed9.chunk.js"
+  },
+  {
+    "revision": "1f8148264d49ef73db93",
+    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/runtime~main.31030840.js"
+  }
+]);
