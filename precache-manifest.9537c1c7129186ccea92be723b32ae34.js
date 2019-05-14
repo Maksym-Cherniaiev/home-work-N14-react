979d3ea47510960e8365ab4173a1fac03af9dc90
Maksym <likeran2@gmail.com>
2019-05-14 23:45:38 +0300

    Updates

--- precache-manifest.9537c1c7129186ccea92be723b32ae34.js
+++ precache-manifest.9537c1c7129186ccea92be723b32ae34.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5555622bf262e1241300cee2a7b824a9",
+    "revision": "f958db6412e547cbef9a8167cb7fd5d2",
     "url": "/Maksym-Cherniaiev/home-work-N14-react.git/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/2.0259027c.chunk.js"
   },
   {
-    "revision": "647f2cdd7801a68d974b",
-    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/main.c6bcfed9.chunk.js"
+    "revision": "ea3f244f2594bd7da238",
+    "url": "/Maksym-Cherniaiev/home-work-N14-react.git/static/js/main.46d52738.chunk.js"
   },
   {
     "revision": "1f8148264d49ef73db93",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Maksym-Cherniaiev/home-work-N14-react.git/precache-manifest.41ed4a7b4e3a146b489ed91431bfe337.js"
+  "/Maksym-Cherniaiev/home-work-N14-react.git/precache-manifest.9537c1c7129186ccea92be723b32ae34.js"
 );
 
 self.addEventListener('message', (event) => {
